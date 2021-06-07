adb77b43e5491562867b05347536192f3a47b3db
Ganza Ntambuko <ganzantambuko@gmail.com>
2021-06-07 12:15:30 +0200

    initial

---
+++ dashboard/main.js
@@ -0,0 +1,12 @@
+
+
+function toggleMenu(){
+    let toggle = document.querySelector('.toggle');
+    toggle.classList.toggle('active')
+
+    let navigation = document.querySelector('.navigation');
+    navigation.classList.toggle('active')
+    let main = document.querySelector('.main');
+    main.classList.toggle('active')
+}
+
